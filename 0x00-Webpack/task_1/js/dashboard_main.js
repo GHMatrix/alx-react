bd73c85f88111bb11f0a737401b009149800b22e
GHMatrix <gsmensah@yahoo.com>
2024-01-02 19:18:12 +0000

    Learning how to use Webpack with a config file

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,22 +1,17 @@
-// js/dashboard_main.js
 import $ from 'jquery';
 import _ from 'lodash';
 
-let count = 0;
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
+
+let conunt = 0;
 
 function updateCounter() {
-  count += 1;
-  $('#count').text(`${count} clicks on the button`);
-}
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
 
-$(document).ready(function () {
-  // Add the specified elements to the body
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<button id="startButton">Click here to get started</button>');
-  $('body').append('<p id="count"></p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-
-  // Bind the debounce function to the click event on the button
-  $('#startButton').on('click', _.debounce(updateCounter, 1000));
-});
+$('button').on('click', _.debounce(updateCounter, 500));
