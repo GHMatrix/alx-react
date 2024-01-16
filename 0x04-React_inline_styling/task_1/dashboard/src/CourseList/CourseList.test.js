3219009414c73b787006f91f9a9180078d4620c1
GHMatrix <gsmensah@yahoo.com>
2024-01-16 15:28:36 +0000

    install Aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/CourseList/CourseList.test.js
@@ -1,29 +1,49 @@
-import React from 'react';
-import CourseList from './CourseList';
-import { render, screen } from '@testing-library/react';
+import React from "react";
+import CourseList from "./CourseList";
+import CourseListRow from "./CourseListRow";
+import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
 
-describe('CourseList Component', () => {
-  test('renders correctly with an empty listCourses array', () => {
-    render(<CourseList />);
-    expect(screen.getByText('No course available yet')).toBeInTheDocument();
-    expect(screen.queryByText('Course name')).toBeNull();
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
+
+const listCourses = [
+  { id: 1, name: "ES6", credit: 60 },
+  { id: 2, name: "Webpack", credit: 20 },
+  { id: 3, name: "React", credit: 40 },
+];
+
+describe("CourseList component tests", () => {
+  it("should render without crashing", () => {
+    const wrapper = shallow(<CourseList />);
+
+    expect(wrapper.exists()).toBe(true);
   });
 
-  test('renders listCourses correctly', () => {
-    const listCourses = [
-      { id: 1, name: 'ES6', credit: 60 },
-      { id: 2, name: 'Webpack', credit: 20 },
-      { id: 3, name: 'React', credit: 40 },
-    ];
+  it("renders 5 different rows", () => {
+    const wrapper = shallow(<CourseList listCourses={listCourses} />);
 
-    render(<CourseList listCourses={listCourses} />);
-    
-    listCourses.forEach(course => {
-      expect(screen.getByText(course.name)).toBeInTheDocument();
-      expect(screen.getByText(course.credit.toString())).toBeInTheDocument();
+    expect(wrapper.find("thead").children()).toHaveLength(2);
+    wrapper.find("thead").forEach((node) => {
+      expect(node.equals(<CourseListRow textFirstCell="Course name" textSecondCell="Credit" isHeader={true} />));
     });
 
-    expect(screen.getByText('Course name')).toBeInTheDocument();
-    expect(screen.queryByText('No course available yet')).toBeNull();
+    expect(wrapper.find("tbody").children()).toHaveLength(3);
+    expect(wrapper.find("tbody").childAt(0).html()).toEqual('<tr style="background-color:#f5f5f5ab"><td>ES6</td><td>60</td></tr>');
+    expect(wrapper.find("tbody").childAt(1).html()).toEqual('<tr style="background-color:#f5f5f5ab"><td>Webpack</td><td>20</td></tr>');
+    expect(wrapper.find("tbody").childAt(2).html()).toEqual('<tr style="background-color:#f5f5f5ab"><td>React</td><td>40</td></tr>');
+  });
+
+  it("renders correctely when passed a list of courses", () => {
+    const wrapper = shallow(<CourseList listCourses={listCourses} />);
+
+    expect(wrapper.find("tbody").children()).toHaveLength(3);
+    expect(wrapper.find("tbody").childAt(0).html()).toEqual('<tr style="background-color:#f5f5f5ab"><td>ES6</td><td>60</td></tr>');
+    expect(wrapper.find("tbody").childAt(1).html()).toEqual('<tr style="background-color:#f5f5f5ab"><td>Webpack</td><td>20</td></tr>');
+    expect(wrapper.find("tbody").childAt(2).html()).toEqual('<tr style="background-color:#f5f5f5ab"><td>React</td><td>40</td></tr>');
   });
 });
