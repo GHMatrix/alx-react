b7d683d094135434f4fba9e295d6966c672f1024
GHMatrix <gsmensah@yahoo.com>
2024-01-16 15:29:48 +0000

    install Aphrodite

--- 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
+++ 0x04-React_inline_styling/task_1/dashboard/src/Header/Header.test.js
@@ -1,16 +1,23 @@
 import React from "react";
 import Header from "./Header";
-import { render, screen } from '@testing-library/react';
+import { shallow } from "enzyme";
+import { StyleSheetTestUtils } from "aphrodite";
+
+beforeEach(() => {
+  StyleSheetTestUtils.suppressStyleInjection();
+});
+afterEach(() => {
+  StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+});
 
 describe("Header", () => {
-    it("renders without crashing", () => {
-        render(<Header />);
-        expect(screen.getByRole('banner')).toBeInTheDocument();
-    });
-
-    it("should render a h1", () => {
-        render(<Header />);
-        expect(screen.getByRole('img')).toBeInTheDocument();
-        expect(screen.getByText('School dashboard')).toBeInTheDocument();
-    });
+  it("render without crashing", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists()).toEqual(true);
+  });
+  it("should render a h1", () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.exists("img")).toEqual(true);
+    expect(wrapper.containsMatchingElement(<h1>School dashboard</h1>)).toEqual(true);
+  });
 });
