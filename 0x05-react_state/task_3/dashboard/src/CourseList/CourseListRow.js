afdba9ce90cb7575a4d8d7d9079798f83b483baa
GHMatrix <gsmensah@yahoo.com>
2024-01-22 15:44:29 +0000

    context consumer & advanced state

---
+++ 0x05-react_state/task_3/dashboard/src/CourseList/CourseListRow.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { StyleSheet, css } from 'aphrodite';
+import CourseShape from './CourseShape';
+
+const styles = StyleSheet.create({
+  headerRow: {
+    backgroundColor: '#deb5b545',
+  },
+  defaultRow: {
+    backgroundColor: '#f5f5f5ab',
+  },
+  th: {
+    padding: '10px',
+  },
+});
+
+function CourseListRow({ isHeader = false, course }) {
+  if (isHeader) {
+    if (!course.credit) {
+      return (
+        <tr className={css(styles.headerRow)}>
+          <th className={css(styles.th)} colSpan="2">{course.name}</th>
+        </tr>
+      );
+    } else {
+      return (
+        <tr className={css(styles.headerRow)}>
+          <th className={css(styles.th)}>{course.name}</th>
+          <th className={css(styles.th)}>{course.credit}</th>
+        </tr>
+      );
+    }
+  } else {
+    return (
+      <tr className={css(styles.defaultRow)}>
+        <td className={css(styles.th)}>{course.name}</td>
+        <td className={css(styles.th)}>{course.credit}</td>
+      </tr>
+    );
+  }
+}
+
+CourseListRow.propTypes = {
+  isHeader: PropTypes.bool,
+  course: PropTypes.shape({
+    id: PropTypes.number.isRequired,
+    name: PropTypes.string.isRequired,
+    credit: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  }).isRequired,
+};
+
+export default CourseListRow;
