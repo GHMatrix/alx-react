a70c759bfa3f73350f4ceb6dbd6a3a7f5465a7d1
GHMatrix <gsmensah@yahoo.com>
2024-02-07 16:06:30 +0000

    Normalizr & Immutable

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.js
@@ -1,27 +1,20 @@
-const initialState = {
-  filter: NotificationTypeFilters.DEFAULT,
-  notifications: []
-};
+import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from '../actions/notificationActionTypes';
+import { Map } from 'immutable';
+import { notificationsNormalizer } from '../schema/notifications';
+
+const initialState = Map({
+  filter: 'DEFAULT',
+  notifications: Map()
+});
 
 const notificationReducer = (state = initialState, action) => {
   switch (action.type) {
     case FETCH_NOTIFICATIONS_SUCCESS:
-      return {
-        ...state,
-        notifications: action.data.map(notification => ({ ...notification, isRead: false }))
-      };
-    case MARK_AS_READ:
-      return {
-        ...state,
-        notifications: state.notifications.map((notification, index) =>
-          index === action.index ? { ...notification, isRead: true } : notification
-        )
-      };
+      return state.merge(notificationsNormalizer(action.data).entities.notifications);
     case SET_TYPE_FILTER:
-      return {
-        ...state,
-        filter: action.filter
-      };
+      return state.set('filter', action.filter);
+    case MARK_AS_READ:
+      return state.setIn(['notifications', action.index, 'isRead'], true);
     default:
       return state;
   }
