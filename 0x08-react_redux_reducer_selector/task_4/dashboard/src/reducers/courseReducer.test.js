dd85f883a563b6db2576424a69e8052e768835e0
GHMatrix <gsmensah@yahoo.com>
2024-02-07 21:40:40 +0000

    Normalizr & Immutable

--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/courseReducer.test.js
@@ -1,9 +1,11 @@
-import courseReducer from './courseReducer';
+import courseReducer from '../reducers/courseReducer';
 import { FETCH_COURSE_SUCCESS, SELECT_COURSE, UNSELECT_COURSE } from '../actions/courseActionTypes';
+import { Map } from 'immutable';
+import { coursesNormalizer } from '../schema/courses';
 
 describe('courseReducer', () => {
   it('should return the initial state', () => {
-    const initialState = [];
+    const initialState = Map();
     expect(courseReducer(undefined, {})).toEqual(initialState);
   });
 
@@ -14,38 +16,38 @@ describe('courseReducer', () => {
       { id: 3, name: "React", credit: 40 }
     ];
     const action = { type: FETCH_COURSE_SUCCESS, data: coursesData };
-    const expectedState = coursesData.map(course => ({ ...course, isSelected: false }));
-    expect(courseReducer([], action)).toEqual(expectedState);
+    const normalizedCourses = coursesNormalizer(coursesData).entities.courses;
+    const expectedState = Map(normalizedCourses);
+    expect(courseReducer(undefined, action)).toEqual(expectedState);
   });
 
   it('should handle SELECT_COURSE', () => {
-    const initialState = [
-      { id: 1, name: "ES6", isSelected: false, credit: 60 },
-      { id: 2, name: "Webpack", isSelected: false, credit: 20 },
-      { id: 3, name: "React", isSelected: false, credit: 40 }
-    ];
-    const action = { type: SELECT_COURSE, index: 1 };
-    const expectedState = [
-      { id: 1, name: "ES6", isSelected: false, credit: 60 },
-      { id: 2, name: "Webpack", isSelected: true, credit: 20 },
-      { id: 3, name: "React", isSelected: false, credit: 40 }
-    ];
+    const initialState = Map({
+      1: { id: 1, name: "ES6", credit: 60, isSelected: false },
+      2: { id: 2, name: "Webpack", credit: 20, isSelected: false },
+      3: { id: 3, name: "React", credit: 40, isSelected: false }
+    });
+    const action = { type: SELECT_COURSE, index: 2 };
+    const expectedState = Map({
+      1: { id: 1, name: "ES6", credit: 60, isSelected: false },
+      2: { id: 2, name: "Webpack", credit: 20, isSelected: true },
+      3: { id: 3, name: "React", credit: 40, isSelected: false }
+    });
     expect(courseReducer(initialState, action)).toEqual(expectedState);
   });
 
   it('should handle UNSELECT_COURSE', () => {
-    const initialState = [
-      { id: 1, name: "ES6", isSelected: false, credit: 60 },
-      { id: 2, name: "Webpack", isSelected: true, credit: 20 },
-      { id: 3, name: "React", isSelected: false, credit: 40 }
-    ];
+    const initialState = Map({
+      1: { id: 1, name: "ES6", credit: 60, isSelected: true },
+      2: { id: 2, name: "Webpack", credit: 20, isSelected: false },
+      3: { id: 3, name: "React", credit: 40, isSelected: false }
+    });
     const action = { type: UNSELECT_COURSE, index: 1 };
-    const expectedState = [
-      { id: 1, name: "ES6", isSelected: false, credit: 60 },
-      { id: 2, name: "Webpack", isSelected: false, credit: 20 },
-      { id: 3, name: "React", isSelected: false, credit: 40 }
-    ];
+    const expectedState = Map({
+      1: { id: 1, name: "ES6", credit: 60, isSelected: false },
+      2: { id: 2, name: "Webpack", credit: 20, isSelected: false },
+      3: { id: 3, name: "React", credit: 40, isSelected: false }
+    });
     expect(courseReducer(initialState, action)).toEqual(expectedState);
   });
 });
-
--- 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.test.js
+++ 0x08-react_redux_reducer_selector/task_4/dashboard/src/reducers/notificationReducer.test.js
@@ -1,9 +1,14 @@
-import notificationReducer from './notificationReducer';
+import notificationReducer from '../reducers/notificationReducer';
 import { FETCH_NOTIFICATIONS_SUCCESS, MARK_AS_READ, SET_TYPE_FILTER } from '../actions/notificationActionTypes';
+import { Map } from 'immutable';
+import { notificationsNormalizer } from '../schema/notifications';
 
 describe('notificationReducer', () => {
   it('should return the initial state', () => {
-    const initialState = { filter: "DEFAULT", notifications: [] };
+    const initialState = Map({
+      filter: 'DEFAULT',
+      notifications: Map()
+    });
     expect(notificationReducer(undefined, {})).toEqual(initialState);
   });
 
@@ -14,39 +19,45 @@ describe('notificationReducer', () => {
       { id: 3, type: "urgent", value: "New data available" }
     ];
     const action = { type: FETCH_NOTIFICATIONS_SUCCESS, data: notificationsData };
-    const expectedState = {
-      filter: "DEFAULT",
-      notifications: notificationsData.map(notification => ({ ...notification, isRead: false }))
-    };
+    const normalizedNotifications = notificationsNormalizer(notificationsData).entities.notifications;
+    const expectedState = Map({
+      filter: 'DEFAULT',
+      notifications: Map(normalizedNotifications)
+    });
     expect(notificationReducer(undefined, action)).toEqual(expectedState);
   });
 
-  it('should handle MARK_AS_READ', () => {
-    const initialState = {
-      filter: "DEFAULT",
-      notifications: [
-        { id: 1, isRead: false, type: "default", value: "New course available" },
-        { id: 2, isRead: false, type: "urgent", value: "New resume available" },
-        { id: 3, isRead: false, type: "urgent", value: "New data available" }
-      ]
-    };
-    const action = { type: MARK_AS_READ, index: 1 };
-    const expectedState = {
-      filter: "DEFAULT",
-      notifications: [
-        { id: 1, isRead: false, type: "default", value: "New course available" },
-        { id: 2, isRead: true, type: "urgent", value: "New resume available" },
-        { id: 3, isRead: false, type: "urgent", value: "New data available" }
-      ]
-    };
+  it('should handle SET_TYPE_FILTER', () => {
+    const initialState = Map({
+      filter: 'DEFAULT',
+      notifications: Map()
+    });
+    const action = { type: SET_TYPE_FILTER, filter: 'URGENT' };
+    const expectedState = Map({
+      filter: 'URGENT',
+      notifications: Map()
+    });
     expect(notificationReducer(initialState, action)).toEqual(expectedState);
   });
 
-  it('should handle SET_TYPE_FILTER', () => {
-    const initialState = { filter: "DEFAULT", notifications: [] };
-    const action = { type: SET_TYPE_FILTER, filter: "URGENT" };
-    const expectedState = { filter: "URGENT", notifications: [] };
+  it('should handle MARK_AS_READ', () => {
+    const initialState = Map({
+      filter: 'DEFAULT',
+      notifications: Map({
+        1: { id: 1, isRead: false, type: "default", value: "New course available" },
+        2: { id: 2, isRead: false, type: "urgent", value: "New resume available" },
+        3: { id: 3, isRead: false, type: "urgent", value: "New data available" }
+      })
+    });
+    const action = { type: MARK_AS_READ, index: 2 };
+    const expectedState = Map({
+      filter: 'DEFAULT',
+      notifications: Map({
+        1: { id: 1, isRead: false, type: "default", value: "New course available" },
+        2: { id: 2, isRead: true, type: "urgent", value: "New resume available" },
+        3: { id: 3, isRead: false, type: "urgent", value: "New data available" }
+      })
+    });
     expect(notificationReducer(initialState, action)).toEqual(expectedState);
   });
 });
-
